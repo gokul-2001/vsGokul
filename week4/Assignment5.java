cd61bc83074237a97862e90bf33dea15eafd2c3e
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2023-08-05 09:39:20 +0530

    Add files via upload

--- week4/Assignment5.java
+++ week4/Assignment5.java
@@ -1,5 +1,37 @@
 package week4;
+interface Shapex {
+	public String base = "This is shape1";
+	public void display1();
+}
+interface Shapey extends Shapex {
+	public String base ="This is shape2";
+	public void display2();
+}
+class Shapeg implements Shapey {
+	public String base ="This is shape3";
 
+	@Override
+	public void display1() {
+		// TODO Auto-generated method stub
+		System.out.println("Circle "+Shapex.base);
+		
+	}
+
+	@Override
+	public void display2() {
+		// TODO Auto-generated method stub
+		System.out.println("Circle "+Shapey.base);
+		
+	}
+
+	
+}
 public class Assignment5 {
+	public static void main(String[] args) {
+		Shapeg circle  = new Shapeg();
+		circle.display1();
+		circle.display2();
+	}
+	
 
 }
