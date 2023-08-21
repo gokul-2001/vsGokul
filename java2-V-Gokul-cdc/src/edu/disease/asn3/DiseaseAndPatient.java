fdc726d4f343dc98d32c8957d0e351bd05e7f1e4
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2023-08-21 18:50:29 +0530

    Add files via upload

---
+++ java2-V-Gokul-cdc/src/edu/disease/asn3/DiseaseAndPatient.java
@@ -0,0 +1,23 @@
+package edu.disease.asn3;
+
+public class DiseaseAndPatient {
+	private Disease[] diseases;
+	private Patient[] patients;
+	
+	
+	public Disease[] getDiseases() {
+		return diseases;
+	}
+	public void setDiseases(Disease[] diseases) {
+		this.diseases = diseases;
+	}
+	public Patient[] getPatients() {
+		return patients;
+	}
+	public void setPatients(Patient[] patients) {
+		this.patients = patients;
+	}
+	
+	
+
+}
