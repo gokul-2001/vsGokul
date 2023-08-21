c7f55b43f28ef7803cf41a4ddc8a8e2d189f3e76
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2023-08-21 22:51:51 +0530

    DiseaseAndPatient.java

--- java2-V-Gokul-cdc/src/edu/disease/asn3/DiseaseAndPatient.java
+++ java2-V-Gokul-cdc/src/edu/disease/asn3/DiseaseAndPatient.java
@@ -1,23 +1,53 @@
 package edu.disease.asn3;
 
+/**
+ * The `DiseaseAndPatient` class represents a container for arrays of `Disease` and `Patient` objects.
+ * It allows encapsulation and management of disease and patient data.
+ */
 public class DiseaseAndPatient {
-	private Disease[] diseases;
-	private Patient[] patients;
-	
-	
-	public Disease[] getDiseases() {
-		return diseases;
-	}
-	public void setDiseases(Disease[] diseases) {
-		this.diseases = diseases;
-	}
-	public Patient[] getPatients() {
-		return patients;
-	}
-	public void setPatients(Patient[] patients) {
-		this.patients = patients;
-	}
-	
-	
+    /**
+     * An array containing `Disease` objects.
+     */
+    private Disease[] diseases;
 
+    /**
+     * An array containing `Patient` objects.
+     */
+    private Patient[] patients;
+
+    /**
+     * Retrieves the array of `Disease` objects.
+     *
+     * @return The array of `Disease` objects.
+     */
+    public Disease[] getDiseases() {
+        return diseases;
+    }
+
+    /**
+     * Sets the array of `Disease` objects.
+     *
+     * @param diseases The array of `Disease` objects to set.
+     */
+    public void setDiseases(Disease[] diseases) {
+        this.diseases = diseases;
+    }
+
+    /**
+     * Retrieves the array of `Patient` objects.
+     *
+     * @return The array of `Patient` objects.
+     */
+    public Patient[] getPatients() {
+        return patients;
+    }
+
+    /**
+     * Sets the array of `Patient` objects.
+     *
+     * @param patients The array of `Patient` objects to set.
+     */
+    public void setPatients(Patient[] patients) {
+        this.patients = patients;
+    }
 }
